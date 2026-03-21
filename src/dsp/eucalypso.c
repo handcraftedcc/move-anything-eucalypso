ee1213673b8beb0acf05a4775ffe41882d76b672
Charles Vestal <charles@charles.pizza>
2026-03-21 13:21:48 +0100

    chore: update paths and references for Schwung rename

--- src/dsp/eucalypso.c
+++ src/dsp/eucalypso.c
@@ -29,7 +29,7 @@
 #define SCALE_BASE_NOTE 60
 #define CLOCK_START_GRACE_TICKS 2
 #define EUCALYPSO_DEBUG_LOG 1
-#define EUCALYPSO_LOG_PATH "/data/UserData/move-anything/eucalypso.log"
+#define EUCALYPSO_LOG_PATH "/data/UserData/schwung/eucalypso.log"
 
 typedef enum {
     PLAY_HOLD = 0,
